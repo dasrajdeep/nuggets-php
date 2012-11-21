4ef6cd79bfec0371748db33cf2f14eedc72feb31
Rajdeep Das <das.rajdeep97@gmail.com>
2012-11-21 22:30:39 +0530

    bug fixed in ext directory change

--- core/Extension.php
+++ core/Extension.php
@@ -74,7 +74,7 @@ class Extension {
 		$args=func_get_args();
 		$args=array_slice($args,1);
 		
-		chdir(self::$extensions[$ext]['directory']);
+		chdir(Registry::getPath('extensions').self::$extensions[$ext]['directory']);
 		
 		$result=call_user_func_array(array($instance,$main),$args);
 		
